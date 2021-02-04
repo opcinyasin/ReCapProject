a0f3bf448b76b00f378358b2d2260395c83ab54c
opcinyasin <yasinop98@gmail.com>
2021-02-04 13:20:12 +0300

    Service changed

---
+++ Business/Abstract/IModelService.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IModelService
+    {
+        void Add(Model model);
+
+
+        void Delete(Model model);
+
+
+        List<Model> GetAll();
+
+
+        void Update(Model model);
+        
+    }
+}
