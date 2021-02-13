bbc99f3593b32411493dceeffbe3530161b52682
opcinyasin <yasinop98@gmail.com>
2021-02-13 22:47:49 +0300

    13.02.2021

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        public List<Car> Get()
+        {
+            return _carService.GetAll().Data;
+        }
+    }
+}
